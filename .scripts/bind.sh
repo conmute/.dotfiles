1db9b8801d4af9f477d5781817e8eadb36b1e7b3
Roman (Ross) Koss <win.maklin@gmail.com>
2024-03-24 23:22:27 +0200

    feat(git): profile and env file setup

--- .scripts/bind.sh
+++ .scripts/bind.sh
@@ -11,3 +11,10 @@ rm ~/.gitignore_global
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.gitignore ~/.gitignore
 ln -s ~/.dotfiles/.gitignore_global ~/.gitignore_global
+
+# User .env config
+mv ~/.env{,.bak}
+ln -s ~/.dotfiles/.env ~/.env
+
+# .dot_profile
+echo "sorce ~/.dotfiles/.dot_profile" >>~/.zshrc
