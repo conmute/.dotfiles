99769558a3b00cb2aa845548da027060d41bbe9f
Roman (Ross) Koss <win.maklin@gmail.com>
2024-03-25 03:45:47 +0200

    chore(conf): trying to enable local configuration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,3 +9,20 @@ end
 vim.print = _G.dd
 
 require("config.lazy")
+
+local projectInit = vim.fn.findfile(".nvim/init.lua", ".;")
+
+if projectInit ~= "" then
+  require(vim.fn.expand("%:p") .. "/.nvim")
+end
+
+-- -- Function to load project-specific init.lua if it exists
+-- local function loadProjectConfig()
+--   local projectInit = vim.fn.findfile(".nvim/init.lua", ".;")
+--   if projectInit ~= "" then
+--     vim.cmd("luafile " .. projectInit)
+--   end
+-- end
+--
+-- -- Autocommand to call loadProjectConfig when entering a directory
+-- vim.cmd("autocmd BufEnter * lua loadProjectConfig()")
---
+++ .nvim/init.lua
@@ -0,0 +1,2 @@
+-- Attempt to setup local configuration
+-- The goal is to, either have local oveerides or additional nvim plugins OR an configuration environment picker…
