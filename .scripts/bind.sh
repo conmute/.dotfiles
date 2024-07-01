9513c5b0e626695d063b7379ad54567449ce5c7e
Roman (Ross) Koss <win.maklin@gmail.com>
2024-07-01 04:59:41 +0300

    feat(bin): moved obsidian scripts to obsidian repo where it belongs

--- .scripts/bind.sh
+++ .scripts/bind.sh
@@ -18,12 +18,6 @@ ln -s $PARENT_DIR/.tmux.conf ~/.tmux.conf
 rm -rf ~/.tmux
 ln -s $PARENT_DIR/.tmux ~/.tmux
 
-# setup obsidian new note
-ln -s $PARENT_DIR/bin/obsidian/on ~/bin/on
-ln -s $PARENT_DIR/bin/obsidian/og ~/bin/og
-ln -s $PARENT_DIR/bin/obsidian/oo ~/bin/oo
-ln -s $PARENT_DIR/bin/obsidian/oinbox ~/bin/oinbox
-
 # setup git config
 rm ~/.gitignore
 rm ~/.gitconfig
